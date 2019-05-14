a911d8642d1f8888ba0e892edebe7ebe05390da1
Roadhog31 <RoadHog31@users.noreply.github.com>
2019-05-14 21:14:56 +0100

    Add project files.

---
+++ UdemyICommandExample/ViewModel/Commands/HelloCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace UdemyICommandExample.ViewModel.Commands
+{
+    public class HelloCommand : ICommand
+    {
+        public ViewModelBase ViewModelBase { get; set; }
+
+        public HelloCommand(ViewModelBase viewModelBase)
+        {
+            ViewModelBase = viewModelBase;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            ViewModelBase.SayHello();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
---
+++ UdemyICommandExample/ViewModel/ViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using UdemyICommandExample.ViewModel.Commands;
+
+namespace UdemyICommandExample.ViewModel
+{
+    public class ViewModelBase
+    {
+        public HelloCommand HelloCommand { get; set; }
+
+        //Class Constructor
+        public ViewModelBase()
+        {
+            HelloCommand = new HelloCommand(this);
+        }
+
+        public void SayHello()
+        {
+            MessageBox.Show("Hello from Commands!");
+        }
+    }
+}
